63eadd2d6f7805507388fb85e6e8dd3d5e9b70b7
Ateng-labrador <arlantengga8@gmail.com>
2026-07-21 21:58:05 +0700

    feat: first commit

---
+++ 02-syntax_basic/TipeData.jl
@@ -0,0 +1,46 @@
+# str
+nama_saya = "Issac Newton"
+email_saya = "IssacNewton@Newton"
+println(nama_saya)
+println(email_saya)
+
+# character
+c = 'A'
+print(C)
+
+# int
+angka_saya = 21
+tahun_lahir = 1998
+println(angka_saya)
+println(tahun_lahir)
+
+# float
+nilai_pi = 3.14159265359
+nilai_phi = 1.61803
+println(nilai_pi)
+println(nilai_phi)
+
+# complex
+z = 3 + 4im #gunakan 'im' literal
+k = complex(3, 4) # gunakan constructor
+
+# julia memakai dynamic + optional static typing
+
+# Array / List
+x = [1, 2, 3]
+println(x)
+
+# Matrix
+A = [
+    1 2
+    3 4
+]
+
+# Tuple
+x = (1, 2, 3)
+
+# Dictionary
+data = {
+    "name"=> "Arlan",
+    "age"=> 20
+}
